99ae9c15e42b0806491d1f43455d3017f1e9d88f
yoonsang Lee <dlrnt1995@gmail.com>
2020-01-10 15:26:02 +0900

    1. Placement for the mid-term presentation is done. TODO: 1. There're must be more presets. 2. Precision of algorithm is needed to measure again.

---
+++ Assets/Scripts/Yoonsang/LocationPresetTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationPresetTracker : MonoBehaviour {
+  [SerializeField] Transform Target;
+  public List<Transform> Prefabs = new List<Transform>();
+  public List<List<Transform>> Presets = new List<List<Transform>>();
+  public bool IsBuilt = false;
+
+  public void BuildLists() {
+    var tfs = Target.GetComponentsInChildren<Transform>();
+
+    for (int i = 0; i < Prefabs.Count; ++i) {
+      var fwd = Instantiate(Prefabs[i]).GetComponentsInChildren<Transform>();
+      foreach (var e in fwd) {
+        e.gameObject.hideFlags = HideFlags.HideAndDontSave;
+      }      
+      Presets.Add(new List<Transform>(fwd));
+    }
+  }
+
+  public void UsePreset(int number) {
+    Target = Presets[number][0];
+  }
+};
