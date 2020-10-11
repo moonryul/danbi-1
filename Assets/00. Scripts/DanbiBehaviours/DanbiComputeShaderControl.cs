db710f8802d22e0e04ef1f9a06d19236bca09b5a
Yoonsang Lee <dlrnt1995@gmail.com>
2020-10-11 16:05:02 +0900

    asset <Editor pro 3> is added. asset rearranged

---
+++ Assets/00. Scripts/DanbiBehaviours/DanbiComputeShaderControl.cs
@@ -0,0 +1,243 @@
+﻿using UnityEngine;
+
+using ComputeBuffersDic = System.Collections.Generic.Dictionary<string, UnityEngine.ComputeBuffer>;
+
+namespace Danbi
+{
+#pragma warning disable 3001
+    [System.Serializable]
+    public class DanbiComputeShaderControl : MonoBehaviour
+    {
+        #region Exposed           
+        [SerializeField, Header("2 by default for the best performance"), Readonly]
+        int MaxNumOfBounce = 2;
+
+        [SerializeField, Readonly]
+        int SamplingThreshold = 30;
+
+        #endregion Exposed
+
+        #region Internal
+        [SerializeField]
+        ComputeShader rayTracingShader;
+
+        Material AddMaterial_ScreenSampling;
+
+        int SamplingCounter;
+
+        DanbiCameraControl CameraControl;
+
+        public RenderTexture resultRT_LowRes;
+
+        public RenderTexture convergedResultRT_HiRes;
+
+        public ComputeBuffersDic buffersDic { get; } = new ComputeBuffersDic();
+
+        public delegate void OnValueChanged();
+        public static OnValueChanged Call_OnValueChanged;
+
+        #endregion Internal    
+
+        #region Event Functions
+
+        void Awake()
+        {
+            // 1. query the hardward it supports the compute shader.
+            if (!SystemInfo.supportsComputeShaders)
+            {
+                Debug.LogError("This machine doesn't support Compute Shader!", this);
+            }
+
+            // 2. Find Compute Shader if it's not assigned.
+            if (rayTracingShader.Null())
+            {
+                rayTracingShader = DanbiComputeShaderHelper.FindComputeShader("DanbiMain");
+            }
+
+            // 3. Initialize the Screen Sampling shader.
+            AddMaterial_ScreenSampling = new Material(Shader.Find("Hidden/AddShader"));
+
+            // 4. Bind the delegates.
+            Call_OnValueChanged += PrepareMeshesAsComputeBuffer;
+            // Call_OnShaderParamsUpdated += SetShaderParams;
+            DanbiUISync.Call_OnPanelUpdate += OnPanelUpdate;
+
+            // 5. Populate kernels index.
+            PopulateKernels();
+
+            CameraControl = GetComponent<DanbiCameraControl>();
+        }
+
+        void Start()
+        {
+            PrepareMeshesAsComputeBuffer();
+        }
+
+        void OnDisable()
+        {
+            Call_OnValueChanged -= PrepareMeshesAsComputeBuffer;
+            // Call_OnShaderParamsUpdated -= SetShaderParams;
+            DanbiUISync.Call_OnPanelUpdate -= OnPanelUpdate;
+
+            foreach (var it in buffersDic)
+            {
+                it.Value.Release();
+            }
+        }
+
+        void Update()
+        {
+            SetShaderParams();
+        }
+
+        #endregion Event Functions
+
+        #region Behaviours
+        void PopulateKernels()
+        {
+            DanbiKernelHelper.AddKernalIndexWithKey(EDanbiKernelKey.Halfsphere_Reflector_Cube_Panorama,
+                rayTracingShader.FindKernel("Halfsphere_Reflector_Cube_Panorama"));
+            // DanbiKernelHelper.AddKernalIndexWithKey(EDanbiKernelKey.Halfsphere_Reflector_Cylinder_Panorama, rayTracingShader.FindKernel("Halfsphere_Reflector_Cylinder_Panorama"));
+            // DanbiKernelHelper.AddKernalIndexWithKey(EDanbiKernelKey.Cone_Reflector_Cube_Panorama, rayTracingShader.FindKernel("Cone_Reflector_Cube_Panorama"));
+            // DanbiKernelHelper.AddKernalIndexWithKey(EDanbiKernelKey.Cone_Reflector_Cylinder_Panorama, rayTracingShader.FindKernel("Cone_Reflector_Cylinder_Panorama"));
+        }
+
+        void OnPanelUpdate(DanbiUIPanelControl control)
+        {
+            if (control is DanbiUIImageGeneratorParametersPanelControl)
+            {
+                var imageGeneratorParamPanel = control as DanbiUIImageGeneratorParametersPanelControl;
+                MaxNumOfBounce = imageGeneratorParamPanel.maxBoundCount;
+                SamplingThreshold = imageGeneratorParamPanel.samplingThreshold;
+                return;
+            }
+
+            // if (control is DanbiUIVideoGeneratorParametersPanelControl)
+            // {
+            //     var videoGeneratorParamPanel = control as DanbiUIVideoGeneratorParametersPanelControl;
+            //     MaxNumOfBounce = videoGeneratorParamPanel.MaximumBoundCount;
+            //     SamplingThreshold = videoGeneratorParamPanel.SamplingThreshold;
+            //     return;
+            // }
+        }
+        void PrepareMeshesAsComputeBuffer()
+        {
+            // Rebuild Prerequisites for the each prewarper settings.
+            var prewarper = FindObjectsOfType<DanbiPrewarperSetting>();
+            foreach (var i in prewarper)
+            {
+                i.Call_OnMeshRebuild?.Invoke(this);
+            }
+        }
+
+        void SetShaderParams()
+        {
+            rayTracingShader.SetVector("_PixelOffset", new Vector2(UnityEngine.Random.value, UnityEngine.Random.value));
+        }
+
+        public void MakePredistortedImage(Texture2D target, (int x, int y) screenResolutions)
+        {
+            // 01. Prepare RenderTextures.
+            DanbiComputeShaderHelper.PrepareRenderTextures(screenResolutions,
+                                                           out SamplingCounter,
+                                                           ref resultRT_LowRes,
+                                                           ref convergedResultRT_HiRes);
+
+            // 02. Prepare the current kernel for connecting Compute Shader.                    
+            int currentKernel = DanbiKernelHelper.CurrentKernelIndex;
+
+            // Set the other parameters.
+            rayTracingShader.SetBuffer(currentKernel, "_DomeData", buffersDic["_DomeData"]);
+            rayTracingShader.SetBuffer(currentKernel, "_PanoramaData", buffersDic["_PanoramaData"]);
+            rayTracingShader.SetInt("_MaxBounce", MaxNumOfBounce);
+            rayTracingShader.SetBuffer(currentKernel, "_Vertices", buffersDic["_Vertices"]);
+            rayTracingShader.SetBuffer(currentKernel, "_Indices", buffersDic["_Indices"]);
+            rayTracingShader.SetBuffer(currentKernel, "_Texcoords", buffersDic["_Texcoords"]);
+
+            // 03. Prepare the translation matrices.
+            CreateProjectionMatrix(screenResolutions);
+
+            // 04. Textures.
+            DanbiComputeShaderHelper.ClearRenderTexture(resultRT_LowRes);
+            rayTracingShader.SetTexture(currentKernel, "_DistortedImage", resultRT_LowRes);
+            rayTracingShader.SetTexture(currentKernel, "_PanoramaImage", target);
+
+            SamplingCounter = 0;
+        }
+
+        // public void MakePredistortedVideo(Texture2D target, (int x, int y) screenResolutions, Camera mainCamRef)
+        // {
+        //     // TODO: fill the body
+        // }
+
+        void CreateProjectionMatrix((int width, int height) screenResolutions)
+        {
+            if (!CameraControl.useCalibration)
+            {
+                rayTracingShader.SetMatrix("_Projection", Camera.main.projectionMatrix);
+                rayTracingShader.SetMatrix("_CameraInverseProjection", Camera.main.projectionMatrix.inverse);
+            }
+            // else
+            // {
+            //     float left = 0.0f;
+            //     float right = screenResolutions.width;
+            //     float bottom = 0.0f;
+            //     float top = screenResolutions.height;
+            //     float near = Camera.main.nearClipPlane;
+            //     float far = Camera.main.farClipPlane;
+
+            //     var openGL_NDC_KMat = DanbiComputeShaderHelper.GetOpenGL_KMatrix(left, right, bottom, top, near, far);
+
+            //     var cameraExternalData = cameraControlRef.CameraInternalData;
+            //     var openCV_NDC_KMat = DanbiComputeShaderHelper.GetOpenCV_KMatrix(cameraExternalData.focalLengthX,
+            //                                                                      cameraExternalData.focalLengthY,
+            //                                                                      cameraExternalData.principalPointX,
+            //                                                                      cameraExternalData.principalPointY,
+            //                                                                      near, far);
+            //     var projMat = openGL_NDC_KMat * openCV_NDC_KMat;
+            //     rayTracingShader.SetMatrix("_Projection", projMat);
+            //     rayTracingShader.SetMatrix("_CameraInverseProjection", projMat.inverse);
+            //     // TODO: Need to decide how we choose the way how we un-distort.
+
+            //     // rayTracingShader.SetBuffer(DanbiKernelHelper.CurrentKernelIndex, "_DanbiCameraExternalData", buffersDic["_DanbiCameraExternalData"]);
+            //     //RTShader.SetVector("_ThresholdIterative", new Vector2())
+            //     //RTShader.SetInt("_IterativeSafeCounter", );
+            //     //RTShader.SetVector("_ThresholdNewTonIterative", );
+            // }
+
+            rayTracingShader.SetMatrix("_CameraToWorldMat", Camera.main.cameraToWorldMatrix);
+        }
+
+        public void Dispatch((int x, int y) threadGroups, RenderTexture dest)
+        {
+            // 01. Check the ray tracing shader is valid.
+            if (rayTracingShader.Null())
+            {
+                Debug.LogError("Ray-tracing shader is invalid!", this);
+            }
+
+            // 02. Dispatch with the current kernel.
+            rayTracingShader.Dispatch(DanbiKernelHelper.CurrentKernelIndex, threadGroups.x, threadGroups.y, 1);
+
+            // 03. Check Screen Sampler and apply it.      
+            AddMaterial_ScreenSampling.SetFloat("_SampleCount", SamplingCounter);
+
+            // 04. Sample the result into the ConvergedResultRT to improve the aliasing quality.
+            Graphics.Blit(resultRT_LowRes, convergedResultRT_HiRes, AddMaterial_ScreenSampling);
+
+            // 05. To improve the resolution of the result RenderTextue, we upscale it in float precision.
+            Graphics.Blit(convergedResultRT_HiRes, dest);
+
+            // 06. Update the sample counts.
+            ++SamplingCounter;
+            if (SamplingCounter > SamplingThreshold)
+            {
+                // TODO;
+                DanbiControl.Call_OnChangeImageRendered?.Invoke(true);
+                SamplingCounter = 0;
+            }
+        }
+        #endregion Behaviours
+
+    }; // class ending.
+}; // namespace Danbi
---
+++ Assets/00. Scripts/DanbiBehaviours/UIBehaviour/Panel/Panorama/DanbiUIPanoramaScreenOpticalPanelControl.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Danbi
+{
+#pragma warning disable 3001
+    public class DanbiUIPanoramaScreenOpticalPanelControl : DanbiUIPanelControl
+    {
+        [Readonly]
+        public DanbiUIPanoramaCubeOptical Cube;
+        [Readonly]
+        public DanbiUIPanoramaCylinderOptical Cylinder;
+        int prevSelectedPanel = 0;
+        int selectedPanel = 0;
+        new GameObject[] Panel = new GameObject[2];
+
+        public delegate void OnTypeChanged(int selectedPanel);
+        public static OnTypeChanged Call_OnTypeChanged;
+
+        void Start()
+        {
+            Cube = new DanbiUIPanoramaCubeOptical(this);
+            Cylinder = new DanbiUIPanoramaCylinderOptical(this);
+            
+            for (int i = 0; i < 2; ++i)
+            {
+                Panel[i] = transform.GetChild(i + 1).gameObject;
+                if (!Panel[i].name.Contains("Panel"))
+                {
+                    Panel[i] = null;
+                }
+                else
+                {
+                    var parentSize = transform.parent.GetComponent<RectTransform>().rect;
+                    Panel[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(parentSize.width, 0);
+                }
+            }
+
+            for (int i = 0; i < Panel.Length; ++i)
+            {
+                Panel[i].gameObject.SetActive(false);
+            }
+
+            Cube.BindInput(Panel[0].transform);
+            Cylinder.BindInput(Panel[1].transform);
+
+            Call_OnTypeChanged += Caller_OnTypeChanged;
+        }
+
+        protected override void SaveValues()
+        {
+            PlayerPrefs.SetFloat("PanoramaCubeOptical-specularR", Cube.specularR);
+            PlayerPrefs.SetFloat("PanoramaCubeOptical-specularG", Cube.specularG);
+            PlayerPrefs.SetFloat("PanoramaCubeOptical-specularB", Cube.specularB);
+            PlayerPrefs.SetFloat("PanoramaCubeOptical-emissionR", Cube.emissionR);
+            PlayerPrefs.SetFloat("PanoramaCubeOptical-emissionG", Cube.emissionG);
+            PlayerPrefs.SetFloat("PanoramaCubeOptical-emissionB", Cube.emissionB);
+
+            PlayerPrefs.SetFloat("PanoramaCylinderOptical-specularR", Cylinder.specularR);
+            PlayerPrefs.SetFloat("PanoramaCylinderOptical-specularG", Cylinder.specularG);
+            PlayerPrefs.SetFloat("PanoramaCylinderOptical-specularB", Cylinder.specularB);
+            PlayerPrefs.SetFloat("PanoramaCylinderOptical-emissionR", Cylinder.emissionR);
+            PlayerPrefs.SetFloat("PanoramaCylinderOptical-emissionG", Cylinder.emissionG);
+            PlayerPrefs.SetFloat("PanoramaCylinderOptical-emissionB", Cylinder.emissionB);
+        }
+
+        void Caller_OnTypeChanged(int selectedPanel)
+        {
+            prevSelectedPanel = this.selectedPanel;
+            this.selectedPanel = selectedPanel;
+
+            for (int i = 0; i < Panel.Length; ++i)
+            {
+                Panel[i].gameObject.SetActive(false);
+            }
+        }
+
+        public override void OnMenuButtonSelected(Stack<Transform> lastClicked)
+        {
+            if (isPanelOpened)
+            {
+                if (lastClicked.Count > 0)
+                {
+                    lastClicked.Pop();
+                }
+            }
+
+            Panel[prevSelectedPanel].SetActive(false);
+            isPanelOpened = !isPanelOpened;
+            Panel[selectedPanel].SetActive(isPanelOpened);
+        }
+    };
+};
